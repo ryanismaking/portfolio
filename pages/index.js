3ee5013df9180cb3bc2ab912266aa72e7134ed30
Ryan Tessier <tessier.ryan.m@gmail.com>
2021-03-09 17:38:41 +1100

    Include description and avatar

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,44 @@
+import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
+
+const Avatar = () => (
+  <Image
+    src="/images/ryan.png" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Ryan Tessier"
+  />
+)
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Ryan is Making</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <Link href="/">Ryan Tessier</Link>
         </h1>
 
+        <Avatar/>
+
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        I’m a passionate product designer with a dark secret: I used to be a developer. I love Star Trek, Nintendo, teaching group fitness dance classes, and escaping rooms.
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h2>Featured work</h2>
+        <ul>
+          <li>Work piece A.</li>
+          <li>Work piece B.</li>
+        </ul>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+          Last updated recently.
       </footer>
 
       <style jsx>{`
@@ -81,7 +63,6 @@ export default function Home() {
         footer {
           width: 100%;
           height: 100px;
-          border-top: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
           align-items: center;
